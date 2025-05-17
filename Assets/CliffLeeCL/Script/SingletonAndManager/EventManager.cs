0c1fe314e1966c51194e8d7810b043972858d0a7
Cliff Lee CL <tommyboys0107@gmail.com>
2025-05-18 00:35:33 +0800

    [Modify] 刪掉多餘的事件

--- Assets/CliffLeeCL/Script/SingletonAndManager/EventManager.cs
+++ Assets/CliffLeeCL/Script/SingletonAndManager/EventManager.cs
@@ -28,11 +28,6 @@ namespace CliffLeeCL
     public class EventManager : Singleton<EventManager>
     {
         public event Action onGameOver;
-        public event Action onHeroGetFood;
-        public event Action onHeroGetWaterEnergy;
-        public event Action onHeroGetPowerEnergy;
-        public event Action<int> onHeroUseWaterSkill;
-        public event Action<int> onHeroUsePowerSkill;
         public event Action<float> onSaturationLevelChanged;
         public event Action<int> onWaterEnergyChanged;
         public event Action<int> onPowerEnergyChanged;
@@ -44,40 +39,9 @@ namespace CliffLeeCL
             Debug.Log("On Game Over");
         }
 
-        public void OnHeroGetFood()
-        {
-            onHeroGetFood?.Invoke();
-            Debug.Log("On Hero Get Food");
-        }
-
-        public void OnHeroGetWaterEnergy()
-        {
-            onHeroGetWaterEnergy?.Invoke();
-            Debug.Log("On Hero Get Water");
-        }
-
-        public void OnHeroGetPowerEnergy()
-        {
-            onHeroGetPowerEnergy?.Invoke();
-            Debug.Log("On Hero Get Power");
-        }
-
-        public void OnHeroUseWaterSkill(int leftEnergy)
-        {
-            onHeroUseWaterSkill?.Invoke(leftEnergy);
-            Debug.Log($"On Hero Use Water, left {leftEnergy}");
-        }
-
-        public void OnHeroUsePowerSkill(int leftEnergy)
-        {
-            onHeroUsePowerSkill?.Invoke(leftEnergy);
-            Debug.Log($"On Hero Use Power, left {leftEnergy}");
-        }
-
         public void OnSaturationLevelChanged(float finalValue)
         {
             onSaturationLevelChanged?.Invoke(finalValue);
-            Debug.Log($"On Saturation Level Changed, final {finalValue}");
         }
 
         public void OnWaterEnergyChanged(int finalValue)
